30fafa75407d7149df15f6bfd66c7bd1ce314cb2
desengph <philipp.senger@ifm.com>
2021-11-23 18:21:18 +0100

    add load example

--- examples/compute_time.py
+++ examples/compute_time.py
@@ -6,32 +6,49 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    # read timestamps from text file
-    lines = []
+    # read timestamps no load from text file
+    lines_noload = []
     with open('examples/timediff.out') as f:
-        lines = f.readlines()
+        lines_noload = f.readlines()
+    timestmp_noload = []
+    for line in lines_noload:
+        timestmp_noload.append(line.split("\n")[0])
 
-    timestmp = []
-    for line in lines:
-        timestmp.append(line.split("\n")[0])
-        
-    timestmp_array = np.asarray(timestmp, dtype=np.float64)
-    framecount = np.arange(0, timestmp_array.shape[0])
+
+    # read timestamps with load from text file
+    lines_load = []
+    with open('examples/timediff_all.out') as f:
+        lines_load = f.readlines()
+    timestmp_load = []
+    for line in lines_load:
+        timestmp_load.append(line.split("\n")[0])
+
+
+    timestmp_array_noload = np.asarray(timestmp_noload, dtype=np.float64)
+    framecount_noload = np.arange(0, timestmp_array_noload.shape[0])
+    timestmp_array_load = np.asarray(timestmp_load, dtype=np.float64)
+    framecount_load = np.arange(0, timestmp_array_load.shape[0])
 
     # jitter from consecutive timestamps
-    timediff_array = np.diff(timestmp_array)*1e3
-    timediff_array_second_order = timediff_array-np.median(timediff_array)
+    timediff_array_noload = np.diff(timestmp_array_load)*1e3
+    timediff_array_second_order = timediff_array_noload-np.median(timediff_array_noload)
 
+    # print to image
     begin = 0
-    end = timestmp_array.shape[0]
-    fig = plt.figure(figsize=(9, 4))
-    # plt.plot(framecount[begin:end], timediff_array[begin:end])
-    plt.plot(framecount[begin:end], timestmp_array[begin:end])
+    end = 1000
+    fig = plt.figure(figsize=(12, 9))
+    plt.subplot(2, 1, 1)
+    plt.plot(framecount_noload[begin:end], timestmp_array_noload[begin:end])
     plt.xlabel('frame count')
     plt.ylabel('timestamp diff in mu sec')
-    plt.title('ifm3d data retrieval and ROS 2 publishing duration')
+    plt.title('ifm3d data retrieval and ROS 2 publishing duration - no load, single subscriber')
+    plt.subplot(2, 1, 2)
+    plt.plot(framecount_load[begin:end], timestmp_array_load[begin:end])
+    plt.xlabel('frame count')
+    plt.ylabel('timestamp diff in mu sec')
+    plt.title('ifm3d data retrieval and ROS 2 publishing duration - large load, all topics subscribed')
     # plt.show()
-    fig.savefig('publishtime.png', dpi=200) 
+    fig.savefig('examples/publishtime.png', dpi=200) 
     
 if __name__ == '__main__':
     # sys.argv[] #pass arguments if given and whatnot
